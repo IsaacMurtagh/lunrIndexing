c98be87e16e293bf9af7b02f9b3683d03e9e433e
Raymond Yang <raymond.yang@centrapay.com>
2021-07-23 13:14:23 +1200

    Read files and add to index

--- main.js
+++ main.js
@@ -1,33 +1,69 @@
 #!/usr/bin/env node
 const lunr = require('lunr');
 const fs = require('fs');
+const Yaml = require('js-yaml');
+const util = require('util');
 
 // read all yaml files 
 // create index with contents
 // do searching on index
 
-// function readYamls() {
-//   fs.
-// }
+function readFiles(dirname, onFileContent, onError) {
+  fs.readdir(dirname, function(err, filenames) {
+    if (err) {
+      onError(err);
+      return;
+    }
+    filenames.forEach(function(filename) {
+      fs.readFile(dirname + filename, 'utf-8', function(err, content) {
+        if (err) {
+          onError(err);
+          return;
+        }
+        onFileContent(filename, content);
+      });
+    });
+  });
+}
+// builder.add({
+//   "title": "Twelfth-Night",
+//   "body": "If music be the food of love, play on: Give me excess of it…",
+//   "author": "William Shakespeare",
+//   "id": "dev-smoke-test-merchant.yml"
+// })
+// builder.add({
+//   "title": "Twelfth-Night",
+//   "body": "If music be the one food of love, play on: Give me excess of it…",
+//   "author": "William Shakespeare",
+//   "id": "2"
+// })
 
-const index = lunr(function () {
-  this.field('title')
-  this.field('body')
-  this.field('author')
+async function main() {
+  const builder = new lunr.Builder()
+  builder.field('tradingName')
+  
+  const readFilesPromise = util.promisify(readFiles)
+  await readFilesPromise(
+    'merchants/',
+    (filename, content) => {
+      builder.add({
+        ...Yaml.load(content),
+        id: filename
+      })
+    },
+    (err) => console.error(err)
+  );
+  const index = builder.build()
+  console.log('raymond', index);
+  console.log(index.search("Dev Smoke Test Merchant"));
+  
+  // await setTimeout(() => {
+  //   const index = builder.build()
+  //   console.log('raymond', index);
+  //   console.log(index.search("Dev Smoke Test Merchant"));
+  // }, 1000)
+  
+  
+}
 
-  this.add({
-    "title": "Twelfth-Night",
-    "body": "If music be the food of love, play on: Give me excess of it…",
-    "author": "William Shakespeare",
-    "id": "1"
-  })
-
-  this.add({
-    "title": "Twelfth-Night",
-    "body": "If music be the food of love, play on: Give me excess of it…",
-    "author": "William Shakespeare",
-    "id": "2"
-  })
-})
-
-console.log(index.search("music be the food"));
+main()
